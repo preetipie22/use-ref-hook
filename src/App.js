102740fbb77092fb0c54aba8f1bd34afafc681fc
Preeti <preeti.tiwari@fibonalabs.com>
2023-07-26 17:33:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [counter, setCounter] = useState(0);
+  const inputEl = useRef("");
+  const prevCounterRef = useRef("");
+
+  useEffect(() => {
+    prevCounterRef.current = counter
+  }, [counter]);
+
+  console.log(inputEl);
+  const resetButton = () => {
+    setName("");
+    inputEl.current.focus();
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        name="name"
+        autoComplete="off"
+        value={name}
+        ref={inputEl}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={resetButton}>Reset</button>
+      <h1>My name is {name}</h1>
+      <hr />
+      <h1>Random Counter : {counter}</h1>
+      {typeof prevCounterRef.current !== "undefined" && (
+        <h2>Previous Counter : {prevCounterRef.current}</h2>
+        )}
+      <button onClick={(e) => setCounter(Math.ceil(Math.random() * 100))}>Generate Number</button>
     </div>
   );
 }
